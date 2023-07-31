d7662d4fcaa27f02772aec493f9cb93130809d95
Wilson Wang <wilson.wang@bytedance.com>
2023-07-31 14:08:29 -0700

    add dockerfile for warrior build
    Signed-off-by: Wilson Wang <wilson.wang@bytedance.com>

---
+++ docker/wasm/README.md
@@ -0,0 +1,9 @@
+# Docker Image for Building Warrior(WASM-on-Ray)
+
+## Command
+
+```bash
+
+docker build -t warrior-dev - < <repo>/docker/wasm/Dockerfile
+
+```
---
+++ wasm/src/cmd/faas_worker.rs
@@ -0,0 +1,65 @@
+// Copyright 2020-2023 The Ray Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use hyper::server::conn::AddrStream;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+#[derive(Clone)]
+struct AppContext {
+    // Whatever data your application needs can go here
+}
+
+async fn handle(
+    context: AppContext,
+    addr: SocketAddr,
+    req: Request<Body>,
+) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Hello World")))
+}
+
+#[tokio::main]
+async fn main() {
+    let context = AppContext {
+        // ...
+    };
+
+    // A `MakeService` that produces a `Service` to handle each connection.
+    let make_service = make_service_fn(move |conn: &AddrStream| {
+        // We have to clone the context to share it with each invocation of
+        // `make_service`. If your data doesn't implement `Clone` consider using
+        // an `std::sync::Arc`.
+        let context = context.clone();
+
+        // You can grab the address of the incoming connection like so.
+        let addr = conn.remote_addr();
+
+        // Create a `Service` for responding to the request.
+        let service = service_fn(move |req| handle(context.clone(), addr, req));
+
+        // Return the service to hyper.
+        async move { Ok::<_, Infallible>(service) }
+    });
+
+    // Run the server like above...
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let server = Server::bind(&addr).serve(make_service);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
